00820a9ddb7a7344cdbcdfeea4f5911c25f2c898
zoglam <anzoor1998@gmail.com>
2021-01-03 06:53:51 +0300

    Blank for start

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import csv
 
-from aiogram import Bot
+from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
 
 
-BOT_TOKEN = '944547365:AAH67D-oXzAtRXdYX5SsZtBFq_m_rOMxaWA'
+BOT_TOKEN = 'token'
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -22,13 +22,38 @@ record_id, name_of_user, tg_id, timestamp, payment_amount
 
 '''
 
-with open('people.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        for e in row:
-            print(e)
 
-try:
-    executor.start_polling(dp)
-except Exception as e:
-    print('Error while polling: {}'.format(e))
+@dp.message_handler(commands=["start"])
+async def start_command(msg: types.Message):
+    start_msg = '''
+Bot Help:
+Send *<payment_amount>* to search for user info'''
+    await msg.answer(start_msg, parse_mode="markdown")
+
+
+@dp.message_handler()
+async def handle_message(msg: types.Message):
+    if msg.text.isdigit():
+        # TODO worker_func(msg.text)
+        await msg.answer(
+            'Thanks for payment.',
+            parse_mode="markdown"
+        )
+    else:
+        await msg.answer(
+            'Not found.',
+            parse_mode="markdown"
+        )
+
+if __name__ == "__main__":
+    try:
+        executor.start_polling(dp)
+    except Exception as e:
+        print('Error while polling: {}'.format(e))
+
+
+# with open('people.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         for e in row:
+#             print(e)
