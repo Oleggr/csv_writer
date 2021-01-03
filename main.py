e8d4d8509997ef39a22d5c78c0e7fce27a9fa799
zoglam <anzoor1998@gmail.com>
2021-01-03 07:27:29 +0300

    dept command

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+import csv, re
 
 from aiogram import Bot, types
 from aiogram.utils import executor
@@ -23,6 +23,17 @@ Bot Help:
 Send *<payment_amount>* to insert your new payment'''
     await msg.answer(start_msg, parse_mode="markdown")
 
+@dp.message_handler(commands=["dept"])
+async def start_command(msg: types.Message):
+    patt = r'(?P<name>[\wА-Яа-я]+)\s*,\s*(?P<payment>\d+)'
+    values = re.search(patt, msg.text)
+    if patt is not None:
+        name, paymant = values.group('name'), values.group('payment')
+        # TODO fucntion to write values
+        await msg.answer('Success')
+    else:
+        await msg.answer('Nothing to write')
+
 @dp.message_handler()
 async def handle_message(msg: types.Message):
     if msg.text.isdigit():
@@ -42,7 +53,14 @@ async def handle_message(msg: types.Message):
         )
 
 if __name__ == "__main__":
-    try:
-        executor.start_polling(dp)
-    except Exception as e:
-        print('Error while polling: {}'.format(e))
+    patt = r'(?P<name>[\wА-Яа-я]+)\s*,\s*(?P<payment>\d+)'
+    values = re.search(patt, 'vasya, 21345')
+    if patt is not None:
+        name, paymant = values.group('name'), values.group('payment')
+        print(name, paymant)
+    else:
+        pass
+    # try:
+    #     executor.start_polling(dp)
+    # except Exception as e:
+    #     print('Error while polling: {}'.format(e))
